e58f5d4cedb06d4cdf6ace495e137af05eb24a26
joo-pe <joohyung1002@naver.com>
2021-04-27 20:33:39 +0900

    :sparkles: 결과값 검증

--- src/main/java/BaseballGame.java
+++ src/main/java/BaseballGame.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 public class BaseballGame {
 
     String result = "";
+    int strikeCnt = 0;
+    int bollCnt = 0;
 
     public void init() {
         System.out.println("========= Baseball Game ===========");
+        resultMaker();
 
         while(true) {
             System.out.println("숫자를 입력해주세요 : ");
             Scanner scanner = new Scanner(System.in);
+
+            strikeCnt = 0;
+            bollCnt = 0;
         }
     }
 
@@ -37,9 +43,25 @@ public class BaseballGame {
 
 
     // 결과확인
-    public void confirmResult() {
-
+    public void confirmResult(String inputValue) {
+        String str[] = inputValue.split("");
+        for(int i = 0 ; i < result.length() ; i++) {
+            System.out.println("==== " + result.charAt(i));
+            for(int j = 0; j < str.length ; j++) {
+                System.out.println("*** " + str[i]);
+                if((result.charAt(i)+"").equals(str[j]) ) {
+                    if(i==j) {
+                        System.out.println("####" + strikeCnt);
+                        strikeCnt++;
+                    }else {
+                        System.out.println("+++++" + strikeCnt);
+                        bollCnt++;
+                    }
+                }
+            }
+        }
     }
+
 
     public static void main(String[] args) {
         BaseballGame baseballGame = new BaseballGame();
--- src/test/java/BaseballGameTest.java
+++ src/test/java/BaseballGameTest.java
@@ -1,6 +1,9 @@
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -15,11 +18,23 @@ class BaseballGameTest {
     }
 
     @Test
-    @DisplayName("랜덤숫자 뽑아내기")
+    @DisplayName("랜숫자 뽑아내기")
     void resultMakerTest() {
         baseballGame.resultMaker();
         System.out.println(baseballGame.result);
         assertThat(baseballGame.result).hasSize(3);
     }
 
+
+    @DisplayName("결과확인")
+    @ParameterizedTest
+    @CsvSource(value = {"123:1:0", "352:0:0", "872:0:1", "192:2:0" ,"178:2:0"}, delimiter = ':')
+    void contains(String input, int strikeSu, int bollSu) {
+        baseballGame.result = "198";
+        baseballGame.confirmResult(input);
+        assertThat(baseballGame.strikeCnt).isEqualTo(strikeSu);
+        assertThat(baseballGame.bollCnt).isEqualTo(bollSu);
+        baseballGame.strikeCnt = 0;
+        baseballGame.bollCnt = 0;
+    }
 }
